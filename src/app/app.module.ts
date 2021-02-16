ea6e71920b48026ab07dc3e50a929da0e88efeb2
elhassen <hacenselahy@gmail.com>
2021-02-16 17:50:55 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ShowsComponent } from './shows/shows.component';
+import { DetailedComponent } from './detailed/detailed.component';
+import { AuthComponent } from './auth/auth.component';
+import { AddComponent } from './add/add.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    ShowsComponent,
+    DetailedComponent,
+    AuthComponent,
+    AddComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
